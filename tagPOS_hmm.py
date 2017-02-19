59e8eec2f71d3697bc55293764996290d231e0f9
zhangcshcn <zhangc.sh.cn@gmail.com>
2017-02-18 22:10:59 -0500

    training finished. Next step, decoding!

--- tagPOS_hmm.py
+++ tagPOS_hmm.py
@@ -7,22 +7,79 @@ dataPath = 'WSJ_POS_CORPUS_FOR_STUDENTS/'
 
 class POStagger_HMM(object):
     def __init__(self):
-        self.Pemit = {} # {Pos:{word:count}}
-        self.Ptrans = {} # {Pos:{Pos:count}}
+        self.Pemit = {}     # { Pos : { word : count }}
+        # Ptrans = {}    # { Pos : { Pos : count }}
+        self.Words = {}     # { word : set( Pos ) }
+        self.PosSize = 0 
 
-    def readFile(self,path):
+    # sentence model: 
+    #   START => { 45 POSes } => END
+    # START has a transtion probability. 
+    # Words are case sensitive. 
+    def train(self,path):
         PosPre = 'START'
-        for line in csv.reader(file(path),delimiter='\t'):
+        Ptrans= { PosPre:{} }
+        self.path = path
+        try:
+            ftrain = open(path,'r')
+        except:
+            print 'Error opening the file... Please try again... '
+        # read the file once and 
+        for line in csv.reader(ftrain,delimiter='\t'):
             if len(line) == 0:
-
-                continue
+                Pos = 'END'
+                if PosPre not in Ptrans:
+                    Ptrans[PosPre] = {Pos:1}
+                elif Pos not in Ptrans[PosPre]:
+                    Ptrans[PosPre][Pos] = 1
+                else:
+                    Ptrans[PosPre][Pos] += 1
+                PosPre = 'START'
+                continue            
             word,Pos = line[0],line[1]
-            if Pos not in self.Pemit:
-                self.Pemit[Pos] = {}
-            if word not in self.Pemit[Pos]:
-                self.Pemit[Pos][word] = 0
-            self.Pemit[Pos][word] += 1
 
+            if word not in self.Words:
+                self.Words[word] = set(Pos)
+            else:
+                self.Words[word] |= set(Pos)
+            # emission count
+            if Pos not in self.Pemit:
+                self.Pemit[Pos] = {word:1}
+            elif word not in self.Pemit[Pos]:
+                self.Pemit[Pos][word] = 1
+            else:
+                self.Pemit[Pos][word] += 1
+
+            # transition count
+            if PosPre not in Ptrans:
+                Ptrans[PosPre] = {Pos:1}
+            elif Pos not in Ptrans[PosPre]:
+                Ptrans[PosPre][Pos] = 1
+            else:
+                Ptrans[PosPre][Pos] += 1
+            PosPre = Pos
+
+        self.PosSize = len(self.Pemit)
+        self.label = {Pos:enum for enum, Pos in enumerate(self.Pemit)}
+        self.label.update({'END':self.PosSize,'START':self.PosSize})
+        # transition probabilities
+        self.TransMat = np.zeros([self.PosSize+1,self.PosSize+1])
+        for PosPre in Ptrans:
+            i = self.label[PosPre]
+            for Pos in Ptrans[PosPre]:
+                self.TransMat[i,self.label[Pos]] = Ptrans[PosPre][Pos] 
+        for vec in self.TransMat:
+            vec /= np.sum(vec)
+        # emission probabilities
+        for Pos in self.Pemit:
+            total = sum(self.Pemit[Pos].values())
+            for word in self.Pemit[Pos]:
+                self.Pemit[Pos][word] *= 1./total
+
+
+    def tagSentence(self,path):
+        pass
+        
 
 
 if __name__ == '__main__':
@@ -32,5 +89,13 @@ if __name__ == '__main__':
         filePath = sys.argv[1]
     path = dataPath + filePath
     tagger = POStagger_HMM()
-    tagger.readFile(path)
-        
+    tagger.train(path)
+
+    #sentece = input('Enter a sentence.')
+    '''
+    for Pos in tagger.Ptrans:
+        print sum(tagger.Ptrans[Pos].values())        
+
+    for Pos in tagger.Pemit:
+        print sum(tagger.Pemit[Pos].values())
+    '''
